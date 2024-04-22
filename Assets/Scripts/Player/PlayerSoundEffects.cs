336e13da0c2757fc60e0d658653129b53249f5c4
Cannonanon <ben.nalevanko@gmail.com>
2024-04-22 17:06:30 -0400

    Update PlayerSoundEffects.cs

--- Assets/Scripts/Player/PlayerSoundEffects.cs
+++ Assets/Scripts/Player/PlayerSoundEffects.cs
@@ -17,11 +17,13 @@ public class PlayerSoundEffects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         PlayerMovement pm = GetComponent<PlayerMovement>();
 
         if (Input.GetKeyDown(KeyCode.Space)) audiosrc.Play(); //play sound, always when space is pressed, fix later;
         if (Input.GetMouseButtonDown(0)) audiosrc.Play(); //hit sound
         if(pm.GetIsWalking()) audiosrc.Play();
         if(pm.GetIsSprinting()) audiosrc.Play();
+        */
     }
 }
